f5ecd6737c7dedd714dec60403a4c87093d8ecc2
Tamari Giorgobiani <takogiorgobiani97@gmail.com>
2022-05-02 20:48:49 +0400

    javascript fundamentals if else

--- script.js
+++ script.js
@@ -93,5 +93,94 @@ console.log(ageSarah <= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2000);
-*/
 
+
+const now = 2022;
+const ageJonas = now - 1990;
+const ageSarah = now - 2000;
+
+console.log(now - 1990 > now - 2000);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(averageAge);
+
+const marksweight = 78;
+const johnsweight = 92;
+const marksheight = 1.69;
+const johnsheight = 1.95;
+
+const marksBmi = marksweight / (marksheight * marksheight);
+const johnsBmi = johnsweight / (johnsheight * johnsheight);
+console.log(marksBmi, johnsBmi);
+
+const markHigherBMI = marksBmi > johnsBmi;
+console.log(markHigherBMI);
+
+
+const firstName = "tako"
+const job = "teacher"
+const birthYear = 1997;
+const year = 2022;
+
+const tako = "I'm " + firstName + ",  " + (year - birthYear) + " years old, " + " and a " + job + "!";
+console.log(tako);
+
+const takoNew = `I'm ${firstName}, ${year - birthYear} years old, and a ${job} `;
+console.log(takoNew);
+
+console.log(`just a regular string ...`);
+
+console.log("String with \n\
+multiple \n\
+lines");
+
+console.log(`String
+with
+lines`); 
+
+const age = 15;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+    console.log("Sarah can start driving lessons ");
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young, wait another ${yearsLeft} years :)`);
+} 
+
+const birthYear = 1991;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century); 
+
+// Challange #2
+
+const marksweight = 78;
+const johnsweight = 92;
+const marksheight = 1.69;
+const johnsheight = 1.95;
+
+const marksBmi = marksweight / (marksheight * marksheight);
+const johnsBmi = johnsweight / (johnsheight * johnsheight);
+console.log(marksBmi, johnsBmi);
+
+const markHigherBMI = marksBmi > johnsBmi;
+console.log(`Marks BMI i higher than John's`)
+
+let bmi;
+if (marksBmi > johnsBmi) {
+    console.log(`Marks (${marksBmi}) BMI is higher than John's (${johnsBmi})`);
+} else {
+    console.log(`John's ${johnsBmi} BMI is higher than Mark's ${marksBmi}`);
+} */
