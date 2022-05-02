86a0c869051e79f8a57979f1c7a83a61a50701a6
Tamari Giorgobiani <takogiorgobiani97@gmail.com>
2022-05-02 18:37:50 +0400

    javascript fundamentals 1

--- script.js
+++ script.js
@@ -1,12 +1,97 @@
-let js = 'amazing';
+// let js = 'amazing';
 
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
+// console.log("Jonas");
 
 
-let firstName = "Smith";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Smith";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+// console.log(myFirstJob);
+
+
+// let myCountry = "Georgia";
+// let currentContinent = "Europe";
+// let georgianPopularion = "3,000,000";
+
+// console.log(myCountry);
+// console.log(currentContinent);
+// console.log(georgianPopularion);
+
+
+// Boolean
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 4);
+// console.log(typeof "Tako");
+
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+// error in javascript
+console.log(typeof null);
+
+
+let age = 30;
+age = 31;
+
+// consts cant be changed, consts cant be empty
+const birthYear = 1991;
+
+// not recommended to use
+var job = "programmer";
+job = "teacher";
+
+
+// Math operators
+const now = 2022;
+const ageJonas = now - 1990;
+const ageSarah = now - 2000;
+console.log(ageJonas, ageSarah);
+
+// 2 ** 3 meanse 2 to the power of 3 = 2 * 2 * 2
+console.log(ageJonas * 2, ageSarah / 2, 2 ** 3);
+
+
+const firstName = "John";
+const lastName = "Smith";
+console.log(firstName + " " + lastName);
+
+
+// Assignment operatos
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++;
+x--;
+x--;
+console.log(x);
+
+// Comparison oeprators
+
+console.log(ageJonas > ageSarah);  // <  > <= >=
+console.log(ageSarah <= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2000);
+*/
 
